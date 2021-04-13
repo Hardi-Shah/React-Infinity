da468f06da93f380dbd1bc366addca0f553dfd92
hardi shah <hardishahhardi@gmail.com>
2021-04-13 20:42:21 +0530

    added components

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,11 +1,204 @@
-import React from 'react'
+import React from "react";
+import {
+  ChevronDownGreyIcon,
+  LogoIcon,
+  MenuIcon,
+  NameLogoHIcon,
+  NotificationBellIcon,
+  UserImage01Icon,
+  UserImage02Icon,
+  UserImage03Icon,
+  UserImage04Icon,
+  UserImage05Icon,
+  UserProfileIcon,
+  UserProfileLogoutIcon,
+  UserProfilePasswordIcon,
+  UserProfileSettingIcon,
+} from "../../assets/images";
 
-function index() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand navbar-light fixed-top">
+      <div className="horizontal-logo-div">
+        <img src={NameLogoHIcon} alt="namelogoH" className="horizontal-logo" />
+      </div>
+      <span>
+        <img src={LogoIcon} alt="logo" className="horizontal-logo-mobile" />
+      </span>
+      <a className="navbar-brand d-flex w-50 mr-auto" href='/#'>
+        <span id="openNav" className="toggle-nav">
+          <img src={MenuIcon} alt="menu" className="menu-logo" />
+        </span>
+      </a>
+      <div className="navbar-collapse w-100" id="collapsingNavbar">
+        <ul className="navbar-nav ml-auto navbar-user-details">
+          <li id="notification-icon" className="nav-item bell-icon">
+            <img
+              src={NotificationBellIcon}
+              alt="notification-bell"
+              data-toggle="dropdown"
+              className="bell-logo"
+            />
+            <span className="badge notification-badge">3</span>
+            <ul
+              className="dropdown-menu notifications"
+              role="menu"
+              aria-labelledby="dropdownMenu"
+            >
+              <div className="notification-heading">Notifications</div>
+              <ul>
+                <li className="row">
+                  <div className="col-2">
+                    <img
+                      src={UserImage01Icon}
+                      alt="user-01"
+                      className="founder-logo"
+                    />
+                  </div>
+                  <div className="col-10">
+                    <div className="notification-message">
+                      Suzzeth Bungaos tagged you and 18 others in a post
+                    </div>
+                    <div className="notification-time">Oct 3, 2017 9:00 PM</div>
+                  </div>
+                </li>
+                <li className="row">
+                  <div className="col-2">
+                    <img
+                      src={UserImage02Icon}
+                      alt="user-02"
+                      className="founder-logo"
+                    />
+                  </div>
+                  <div className="col-10">
+                    <div className="notification-message">
+                      Suzzeth Bungaos tagged you and 18 others in a post
+                    </div>
+                    <div className="notification-time">Oct 3, 2017 9:00 PM</div>
+                  </div>
+                </li>
+                <li className="row">
+                  <div className="col-2">
+                    <img
+                      src={UserImage03Icon}
+                      alt="user-03"
+                      className="founder-logo"
+                    />
+                  </div>
+                  <div className="col-10">
+                    <div className="notification-message">
+                      Suzzeth Bungaos tagged you and 18 others in a post
+                    </div>
+                    <div className="notification-time">Oct 3, 2017 9:00 PM</div>
+                  </div>
+                </li>
+                <li className="row">
+                  <div className="col-2">
+                    <img
+                      src={UserImage04Icon}
+                      alt="user-04"
+                      className="founder-logo"
+                    />
+                  </div>
+                  <div className="col-10">
+                    <div className="notification-message">
+                      Suzzeth Bungaos tagged you and 18 others in a post
+                    </div>
+                    <div className="notification-time">Oct 3, 2017 9:00 PM</div>
+                  </div>
+                </li>
+              </ul>
+              <li className="view-all">
+                Show All
+                <span>
+                  <img
+                    src={ChevronDownGreyIcon}
+                    alt="chevron-down-grey"
+                    className="chevron-down-logo"
+                  />
+                </span>
+              </li>
+            </ul>
+          </li>
 
-export default index
+          <li className="nav-item">
+            <img
+              src={UserImage05Icon}
+              alt="user-05"
+              className="user-profile-logo"
+            />
+          </li>
+          <li className="nav-item nav-item-mobile">
+            <div className="founder-name">John Doe</div>
+            <div className="dropdown">
+              <a
+                className="founder-dropdown dropdown-toggle"
+                href="/#"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+              >
+                Founder of app
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <div className="row">
+                  <a className="dropdown-item user-dropdown" href="/#">
+                    <div className="col-2">
+                      <img src={UserProfileIcon} alt="user-profile" />
+                    </div>
+                    <div className="col-10">Profile</div>
+                  </a>
+                </div>
+                <div>
+                  <a className="dropdown-item user-dropdown" href="/#">
+                    <div className="col-2">
+                      <img
+                        src={UserProfileSettingIcon}
+                        alt="user-profile-seting"
+                      />
+                    </div>
+                    <div className="col-10">Settings</div>
+                  </a>
+                </div>
+                <div>
+                  <a className="dropdown-item user-dropdown" href="/#">
+                    <div className="col-2">
+                      <img
+                        src={UserProfilePasswordIcon}
+                        alt="user-profile-password"
+                      />
+                    </div>
+                    <div className="col-10">Change Password</div>
+                  </a>
+                </div>
+                <div>
+                  <a className="dropdown-item user-dropdown" href="/login">
+                    <div className="col-2">
+                      <img src={UserProfileIcon} alt="user-profile" />
+                    </div>
+                    <div className="col-10">Login</div>
+                  </a>
+                </div>
+                <div>
+                  <a className="dropdown-item user-dropdown" href="/#">
+                    <div className="col-2">
+                      <img
+                        src={UserProfileLogoutIcon}
+                        alt="user-profile-logout"
+                      />
+                    </div>
+                    <div className="col-10">Logout</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
+import { AccountingIcon, ComponentsIcon, DashboardIocn, EmailsIcon, HelpIcon, ReportIcon, ShippingIcon } from "../../assets/images";
 
-function index() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Sidebar = () => {
+  return (
+    <div id="Sidebar" className="sidenav">
+      <div className="sidebar-list">
+        <a href="/dashboard">
+          <div className="row">
+            <div className="col-2">
+              <img src={DashboardIocn} alt='sidebar-logo' className="sidebar-logo" />
+            </div>
+            <div className="col-10">Dashboard</div>
+          </div>
+        </a>
+        <a href="/#">
+          <div className="row">
+            <div className="col-2">
+              <img src={ComponentsIcon} alt='component-logo' className="sidebar-logo" />
+            </div>
+            <div className="col-10">Components</div>
+          </div>
+        </a>
+        <a href="/#">
+          <div className="row">
+            <div className="col-2">
+            <img src={EmailsIcon} alt='email-logo' className="sidebar-logo" />
+            </div>
+            <div className="col-10">Emails</div>
+          </div>
+        </a>
+        <a href="/#">
+          <div className="row">
+            <div className="col-2">
+              <img src={AccountingIcon} alt='accounting-logo' className="sidebar-logo" />
+            </div>
+            <div className="col-10">Accounting</div>
+          </div>
+        </a>
+        <a href="/#">
+          <div className="row">
+            <div className="col-2">
+            <img src={ShippingIcon} alt='shipping-logo' className="sidebar-logo" />
+            </div>
+            <div className="col-10">Shipping</div>
+          </div>
+        </a>
+        <a href="/#">
+          <div className="row">
+            <div className="col-2">
+            <img src={HelpIcon} alt='help-logo' className="sidebar-logo" />
+            </div>
+            <div className="col-10">Help</div>
+          </div>
+        </a>
+        <a href="/#">
+          <div className="row">
+            <div className="col-2">
+            <img src={ReportIcon} alt='report-logo' className="sidebar-logo" />
+            </div>
+            <div className="col-10">Report</div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default index
+export default Sidebar;
