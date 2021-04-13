dcf2e5eec8ac4d664cbc3b95f82610425ec3514d
hardi shah <hardishahhardi@gmail.com>
2021-04-13 20:41:56 +0530

    updated file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import InnerPage from "./pages/InnerPage";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/header" component={Header} />
+          <Route exact path="/sidebar" component={Sidebar} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/innerpage" component={InnerPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
